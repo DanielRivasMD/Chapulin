22561e6e5b27acefde9c1412efd405e372c477c5
DanielRivasMD <danielrivasmd@gmail.com>
2020-03-23 06:57:17 +0100

    update main alfa

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,23 @@
 
 mod file_reader;
 mod mobile_elements;
+mod chromosomal_loci;
+mod peak_identification;
 
 fn main() -> std::io::Result<()> {
+
+  // TODO: write pre processing recomendations => fastq filtering, alignment
+
+  // mobile elements module
   mobile_elements::me_controller()?;
+
+  // chromosomal loci module
+  chromosomal_loci::cl_contorller();
+
+  // peak identification module
+  peak_identification::pi_controller();
+
+  // TODO: build interphase to PostgreSQL
+
   Ok(())
 }
