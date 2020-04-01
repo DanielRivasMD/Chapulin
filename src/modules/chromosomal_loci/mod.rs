182de765c0f425fdde0df499e1a96e85593fd8d3
DanielRivasMD <danielrivasmd@gmail.com>
2020-04-01 19:25:48 +0200

    compused directory path

--- src/modules/chromosomal_loci/mod.rs
+++ src/modules/chromosomal_loci/mod.rs
@@ -9,11 +9,18 @@ pub fn cl_controller(
 ) -> std::io::Result<()> {
 
   // load reference chromosome aligned reads
-  let cl_aligned_file = "/Users/drivas/Factorem/Chapulin/test/SAMN01162223.sorted.RemoveSupplementary.MarkDuplicates.sam".to_string();
-  cl_aligned::cl_mapper(
-    &cl_aligned_file,
-    &mut hash_map_collection,
-  ).expect(&cl_aligned_file);
+  for i in 1..3 {
+
+    let directory = "/Users/drivas/chapulinTest/".to_string();
+    let prefix = "SAMN01162223_R".to_string();
+    let sufix = ".sorted.sam".to_string();
+    let cl_aligned_file = format!("{}{}{}{}", directory, prefix, i, sufix);
+
+    cl_aligned::cl_mapper(
+      &cl_aligned_file,
+      &mut hash_map_collection,
+    ).expect(&cl_aligned_file);
+  }
 
   Ok(())
 }
--- src/modules/mobile_elements/mod.rs
+++ src/modules/mobile_elements/mod.rs
@@ -14,8 +14,11 @@ pub fn me_controller(
   // me_registry::me_lib_loader(&me_library_file).expect(&me_library_file);
 
   // load mobile element aligned reads
-  // let me_aligned_file = "/Users/drivas/Factorem/Chapulin/test/head.sam".to_string();
-  let me_aligned_file = "/Users/drivas/Factorem/Chapulin/test/SAMN01162223_ERV_chlSab_XV.sam".to_string();
+  let directory = "/Users/drivas/chapulinTest/".to_string();
+  let prefix = "SAMN01162223_".to_string();
+  let sufix = "ERV_chlSab_XV.sam".to_string();
+  let me_aligned_file = format!("{}{}{}", directory, prefix, sufix);
+
   me_aligned::me_identificator(
     &me_aligned_file,
     &mut hash_map_collection,
