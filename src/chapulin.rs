e4ee795496bf104de1520a746d515cc717c384b1
DanielRivasMD <danielrivasmd@gmail.com>
2020-04-04 17:48:51 +0200

    add regex static

--- src/chapulin.rs
+++ src/chapulin.rs
@@ -4,6 +4,35 @@
 pub mod utils;
 pub mod modules;
 
+#[macro_use]
+extern crate lazy_static;
+
+use regex::Regex;
+// use lazy_static;
+
+lazy_static! {
+
+  // compile regex only once with lazy static
+  pub static ref REX_VEC: Vec<Regex> = vec![
+    Regex::new(
+      r"(?P<clip>\d+[HS])"
+    ).unwrap(),
+
+    Regex::new(
+      r"(?P<clip>\d+M)"
+    ).unwrap(),
+
+    Regex::new(
+      r"(?P<clip>\d+D)"
+    ).unwrap(),
+
+    Regex::new(
+      r"(?P<clip>\d+I)"
+    ).unwrap(),
+  ];
+}
+
+
 pub fn with_love() {
 
   println!("from lib rs with love!");
