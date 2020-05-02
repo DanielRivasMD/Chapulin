fc95a0dd958f6eee4543c6cc5ba0140002da3ecc
DanielRivasMD <danielrivasmd@gmail.com>
2020-05-02 19:36:02 +0200

    add cigar interpretor trait

--- src/utils/cigar.rs
+++ src/utils/cigar.rs
@@ -9,6 +9,7 @@ pub struct CIGAR {
 }
 
 impl CIGAR {
+
   pub fn new() -> Self {
     Self {
       lclip: 0,
@@ -18,4 +19,46 @@ impl CIGAR {
       del: Vec::new(),
     }
   }
+
+  pub fn loader(to_interpret: &String) -> Self {
+    let mut char_vec = vec![];
+
+    for i in to_interpret.char_indices() {
+
+      match i.1 {
+        'H' | 'S' | 'M' | 'D' | 'I' => {
+          char_vec.push(i.0);
+        }
+        _ => (),
+      }
+    }
+
+    let mut this_cigar = CIGAR::new();
+    let mut j = 0;
+    for i in char_vec.iter() {
+
+      match &to_interpret[*i..*i + 1] {
+        "H" | "S" => {
+          if this_cigar.align.iter().sum::<i32>() == 0 {
+            this_cigar.lclip = (&to_interpret[j..*i]).parse::<i32>().unwrap();
+          } else {
+            this_cigar.rclip = (&to_interpret[j..*i]).parse::<i32>().unwrap();
+          }
+        },
+        "M" => {
+          this_cigar.align.push((&to_interpret[j..*i]).parse::<i32>().unwrap());
+        },
+        "I" => {
+          this_cigar.ins.push((&to_interpret[j..*i]).parse::<i32>().unwrap());
+        },
+        "D" => {
+          this_cigar.del.push((&to_interpret[j..*i]).parse::<i32>().unwrap());
+        },
+        _ => {}
+      }
+      j = i + 1;
+    };
+    return this_cigar
+  }
+
 }
