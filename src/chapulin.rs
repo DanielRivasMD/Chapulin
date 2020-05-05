0a37ef5bd2dd2ef2a4260d26c7f043db20017f82
DanielRivasMD <danielrivasmd@gmail.com>
2020-05-05 15:11:11 +0200

    remove regex & lazy static

--- src/chapulin.rs
+++ src/chapulin.rs
@@ -1,36 +1,10 @@
 
 // standard libraries
-#[macro_use]
-extern crate lazy_static;
-use regex::Regex;
 
 // modules
 pub mod settings;
 pub mod utils;
 pub mod modules;
-
-// define regex static
-lazy_static! {
-
-  pub static ref REX_VEC: Vec<Regex> = vec![
-    Regex::new(
-      r"(?P<clip>\d+[HS])"
-    ).unwrap(),
-
-    Regex::new(
-      r"(?P<clip>\d+M)"
-    ).unwrap(),
-
-    Regex::new(
-      r"(?P<clip>\d+D)"
-    ).unwrap(),
-
-    Regex::new(
-      r"(?P<clip>\d+I)"
-    ).unwrap(),
-  ];
-}
-
 
 pub fn with_love() {
 
