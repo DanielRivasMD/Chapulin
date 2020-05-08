afaeaf0e23a26ca6aef44236b9f065072b3a7464
DanielRivasMD <danielrivasmd@gmail.com>
2020-05-08 10:42:37 +0200

    read positioning table

--- src/utils/chranchor_enum.rs
+++ src/utils/chranchor_enum.rs
@@ -5,3 +5,16 @@ pub enum ChrAnchor {
   Read2,
   None,
 }
+
+/*
+
+|---------------------------------------------|---------------------------------------------|
+| C anchor [unmpped] (position) {orientation} | ME anchor [mapped] (position) {orientation} |
+|---------------------------------------------|---------------------------------------------|
+| complete [*] (mate) {inwards}               | complete [100M] (ME limit) {outwards}       |
+| complete [*] (mate) {inwards}               | partial [50S50M] (ME limit) {outwards}      |
+| partial [50S50M] (ME limit) {inwards}       | complete [100M]  (ME limit) {outwards}      |
+| partial [50S50M] (ME limit) {inwards}       | partial [50S50M]  (ME limit) {outwards}     |
+|---------------------------------------------|---------------------------------------------|
+
+ */
