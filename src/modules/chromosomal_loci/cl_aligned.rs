87abaa2296f86044c6350db183677f0cd3b0b212
DanielRivasMD <danielrivasmd@gmail.com>
2020-05-14 16:23:02 +0200

    collect chromosomal max values unclean

--- src/modules/chromosomal_loci/cl_aligned.rs
+++ src/modules/chromosomal_loci/cl_aligned.rs
@@ -20,6 +20,7 @@ pub fn cl_mapper(
   cl_bam_file: &String,
   hm_collection: Arc<Mutex<HashMap<String, ReadRecord>>>,
   an_registry: Arc<Mutex<HashMap<String, Vec<String>>>>,
+  chr_max: Arc<Mutex<HashMap<String, i32>>>,
 ) -> std::io::Result<()> {
 
 // pub fn cl_mapper(
@@ -84,12 +85,50 @@ pub fn cl_mapper(
         if ! an_registry.lock().unwrap().contains_key(record_line[2]) {
         // if ! an_registry.contains_key(record_line[2]) {
           an_registry.lock().unwrap().insert(record_line[2].to_string(), Vec::new());
+          chr_max.lock().unwrap().insert(record_line[2].to_string(), 0);
+
           // an_registry.insert(record_line[2].to_string(), Vec::new());
         }
+
+
+
+        if hm_collection.lock().unwrap().get(record_line[0]).unwrap().read1.chr_read[0].pos > *chr_max.lock().unwrap().get_mut(record_line[2]).unwrap() {
+          chr_max.lock().unwrap().insert(record_line[2].to_string(), hm_collection.lock().unwrap().get(record_line[0]).unwrap().read1.chr_read[0].pos);
+        }
+
+        // let mut tmp_pos = 0;
+        //
+        // if let Some(tmp_entry) = hm_collection.lock().unwrap().get(record_line[0]) {
+        //   tmp_pos = tmp_entry.read1.chr_read[0].pos;
+        // }
+        //
+        // if tmp_pos > chr_max_val {
+        //   chr_max_val = tmp_pos;
+        // }
+
+
+        // if let Some(current_chr) = chr_max.lock().unwrap().get(record_line[2]) {
+        //   if let Some(tmp_entry) = hm_collection.lock().unwrap().get_mut(record_line[0]) {
+        //
+        //     let tmp_bool = tmp_entry.read1.chr_read[0].pos > *current_chr;
+        //
+        //     println!("{} {} {}", current_chr, &tmp_entry.read1.chr_read[0].pos, tmp_bool);
+        //
+        //     if tmp_bool {
+        //       chr_max.lock().unwrap().insert(record_line[2].to_string(), tmp_entry.read1.chr_read[0].pos);
+        //       // current_chr = &mut tmp_entry.read1.chr_read[0].pos;
+        //     }
+        //
+        //
+        //     // chr_max.lock().unwrap().insert(record_line[2].to_string(), tmp_entry.read1.chr_read[0].pos);
+        //   }
+        // }
 
         if let Some(current_chr) = an_registry.lock().unwrap().get_mut(record_line[2]) {
         // if let Some(current_chr) = an_registry.get_mut(record_line[2]) {
           current_chr.push(record_line[0].to_string())
+
+
         }
       }
     }
