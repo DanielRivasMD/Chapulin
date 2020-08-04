2d4d64148014e047174f6e32832f4514a97e8ce6
DanielRivasMD <danielrivasmd@gmail.com>
2020-08-04 18:57:42 +0200

    dale mas color!

--- src/error/config_error.rs
+++ src/error/config_error.rs
@@ -3,28 +3,28 @@
 
 // standard libraries
 use thiserror::Error;
-
+use colored::*;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 #[derive(Debug, Error)]
 pub enum ChapulinConfigError {
-  #[error("\n\nFail to reaad configuration\n\n")]
+  #[error("\n{}\n", "Fail to reaad configuration".red())]
   ConfigHashMap,
-  #[error("\n\nNo configuration file was set:\nSet a configuration file with option '-c --config'\n\n")]
+  #[error("\n\t{}\n{}{}", "No configuration file not was set:".red(), "Set a configuration file with option ", "'-c --config'".cyan(), )]
   EmptyConfigOption,
-  #[error("\n\nConfiguration file not found\n\n")]
+  #[error("\n\t{}", "Configuration file not found".red())]
   NoConfigFile,
-  #[error("\n\nDirectory was not set properly in configuration file\n\nExample: directory = \"/home/favorite_chapulin_directory/\"\n\n")]
+  #[error("\n\t{}\n{}{}", "Directory was not set properly in configuration file".red(), "Example: directory = ", "'/home/favorite_chapulin_directory/'".cyan())]
   BadDirectoryVar,
-  #[error("\n\nReference file was not set properly in configuration file\n\nExample: reference = \"awesome_species_reference.fa\"\n\n")]
+  #[error("\n\t{}\n{}{}", "Reference file was not set properly in configuration file".red(), "Example: reference = ", "'awesome_species_reference.fa'".cyan())]
   BadReferenceVar,
-  #[error("\n\nMobile element library was not set properly in configuration file\n\nExample: mobile_element_library = \"cool_ME_library.txt\"\n\n")]
+  #[error("\n\t{}\n{}{}", "Mobile element library was not set properly in configuration file".red(), "Example: mobile_element_library = ", "'cool_ME_library.txt'".cyan())]
   BadMELibVar,
-  #[error("\n\nMobile element alignment was not set properly in configuration file\n\nExample: mobile_element_alignment = \"ME_alignment_to_awesome_species.sam\"\n\n")]
+  #[error("\n\t{}\n{}{}", "Mobile element alignment was not set properly in configuration file".red(), "Example: mobile_element_alignment = ", "'ME_alignment_to_awesome_species.sam'".cyan())]
   BadMEAlignVar,
-  #[error("\n\nReference genome alignment was not set properly in configuration file\n\nExample: reference_genome_alignment = \"alignment_to_awesome_species_reference_R\"\n\nNote: this is a single-end alignment, therefore files shoud be: \n\t\"alignment_to_awesome_species_reference_R1.sam\" & \"alignment_to_awesome_species_reference_R2.sam\",\nwhere suffixes are infered\n\n")]
+  #[error("\n\t{}\n{}{}", "Reference genome alignment was not set properly in configuration file".red(), "Example: reference_genome_alignment = ", "'alignment_to_awesome_species_reference_R' Note: this is a single-end alignment, therefore files shoud be: 'alignment_to_awesome_species_reference_R1.sam' & 'alignment_to_awesome_species_reference_R2.sam', where suffixes are infered".cyan())]
   BadReferenceGenomeVar,
 }
 
