325a625293d14523deeb33c7583398d67b9f1b0a
DanielRivasMD <danielrivasmd@gmail.com>
2020-08-05 07:40:25 +0200

    deprecated buff reader

--- src/utils/file_reader.rs
+++ src/utils/file_reader.rs
@@ -21,38 +21,4 @@ pub fn byte_file_reader(
   return lines
 }
 
-pub fn buff_file_reader(input_file: &String) -> (CustBufReader, String) {
-  let reader = CustBufReader::open(&input_file)
-    .expect(format!("\n\nProblem opening file:\n\n{}\n\nPossibly file does not exist\n\n", &input_file).as_str());
-  let buffer = String::new();
-  return (reader, buffer)
-}
-
-pub struct CustBufReader {
-  reader: io::BufReader<File>,
-}
-
-impl CustBufReader {
-  pub fn open(
-    path: impl AsRef<std::path::Path>
-  ) -> io::Result<Self> {
-    let file = File::open(path)?;
-    let reader = io::BufReader::new(file);
-
-    Ok(Self { reader })
-  }
-
-  pub fn read_line<'buf>(
-    &mut self,
-    buffer: &'buf mut String,
-  ) -> Option<io::Result<&'buf mut String>> {
-    buffer.clear();
-
-    self.reader
-      .read_line(buffer)
-      .map(|u| if u == 0 { None } else { Some(buffer) })
-      .transpose()
-  }
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
