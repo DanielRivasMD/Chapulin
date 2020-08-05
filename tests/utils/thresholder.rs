b57cafcaf415d3c2b106f4907a5473bcd5825332
DanielRivasMD <danielrivasmd@gmail.com>
2020-08-05 09:36:53 +0200

    use data-test threshold test

--- tests/utils/thresholder.rs
+++ tests/utils/thresholder.rs
@@ -2,32 +2,49 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // standard libraries
-use std::collections::{HashMap};
+use data_test::data_test;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
 use chapulin::settings::constants::NO_FDR;
-use chapulin::utils::thresholder::{thresholder};
+use chapulin::utils::functions::thresholder::thresholder;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-#[test]
-fn test_thresholder() {
-  let mut hm = HashMap::new();
-  hm.insert(1, vec!["uno".to_string(), "dos".to_string(), "tres".to_string(),]);
+data_test! {
 
-  assert_eq!(
-    thresholder(
-      0.,
-      1000.,
-      0.001,
-      &hm,
-      NO_FDR,
-    ),
-    3,
-  );
+  fn test_thresholder(
+    pop_reads,
+    chromosome_size,
+    false_discovery_tolerance,
+    read_hm_k,
+    read_hm_v,
+    psize,
+    expected
+  ) => {
+
+    let mut hm = std::collections::HashMap::new();
+    hm.insert(
+      read_hm_k, 
+      read_hm_v, 
+    );
+
+    assert_eq!(
+      super::thresholder(
+        pop_reads, 
+        chromosome_size, 
+        false_discovery_tolerance, 
+        &hm, 
+        psize, 
+      ),
+      expected
+    );
+  }
+  - talfa (0., 1000., 0.001, 1, vec!["uno".to_string(), "dos".to_string(), "tres".to_string(),], super::NO_FDR, 3)
+  // TODO: add more tests
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
