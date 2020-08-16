0a250e4739d6a9d546bf48ff543ba90299ac0bb0
DanielRivasMD <danielrivasmd@gmail.com>
2020-08-16 17:07:28 +0200

    comment macros

--- src/macros/mc_func.rs
+++ src/macros/mc_func.rs
@@ -3,22 +3,32 @@
 
 /// calculate effective genome length with default parameters: BIN_SIZE & BIN_OVERLAP
 macro_rules! effective_genome_length_calculator {
-  ($gl: expr, $bs: expr, $bo: expr) => {
-    effective_genome_length_calculator($gl, $bs, $bo)
+
+  // explicit values
+  ( $gl: expr, $bs: expr, $bo: expr ) => {
+    effective_genome_length_calculator( $gl, $bs, $bo )
   };
-  ($gl: expr) => {
-    effective_genome_length_calculator($gl, BIN_SIZE as f64, BIN_OVERLAP as f64)
+
+  // default values
+  ( $gl: expr ) => {
+    effective_genome_length_calculator( $gl, BIN_SIZE as f64, BIN_OVERLAP as f64 )
   };
+
 }
 
 /// calculate poisson's lambda default parameters: BIN_SIZE
 macro_rules! lambda_calculator {
-  ($pr: expr, $egl: expr, $bs: expr) => {
-    lambda_calculator($pr, $egl, $bs)
+
+  // explicit values
+  ( $pr: expr, $egl: expr, $bs: expr ) => {
+    lambda_calculator( $pr, $egl, $bs )
   };
-  ($pr: expr, $egl: expr) => {
-    lambda_calculator($pr, $egl, BIN_SIZE as f64)
+
+  // default values
+  ( $pr: expr, $egl: expr ) => {
+    lambda_calculator( $pr, $egl, BIN_SIZE as f64 )
   };
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
