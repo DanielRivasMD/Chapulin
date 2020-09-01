8ee70043a1210a910533caa16f3c5105daac5944
DanielRivasMD <danielrivasmd@gmail.com>
2020-09-01 13:38:18 +0200

    add cache mobile element library

--- src/modules/mobile_elements/mod.rs
+++ src/modules/mobile_elements/mod.rs
@@ -5,11 +5,15 @@
 use std::collections::{HashMap};
 use std::sync::{Arc, Mutex};
 use anyhow::Result as anyResult;
+use std::path::Path;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // modules
-mod me_registry;
+use super::fasta_read::fasta_file_read;
+use super::fasta_read::fasta_cache_read;
+use super::fasta_read::fasta_cache_write;
+
 mod me_aligned;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -28,27 +32,48 @@ pub fn me_controller (
   directory: &str,
   me_library: &str,
   me_aligned_file: &str,
+  hash_map_me_library: Arc<Mutex<HashMap<String, f64>>>,
   hash_map_collection: Arc<Mutex<HashMap<String, MEChimericPair>>>,
 ) -> anyResult<()> {
 
-  // init mobile element library hashmap
-  let mut me_collection = HashMap::new();
-
   // load mobile element library
   let me_library_file = format!("{}{}", directory, me_library);
+  let me_library_cache = format!("{}{}.cache", directory, me_library);
 
-  me_registry::me_lib_loader(
-    &me_library_file,
-    &mut me_collection,
-  )?;
+  if Path::new(&me_library_cache).exists() {
+
+    // read from cache
+    let cr_me_library = hash_map_me_library.clone();
+    fasta_cache_read::read_cache(
+      &me_library_file,
+      cr_me_library,
+    )?;
+
+  } else {
+
+    // read fasta reference
+    let cr_me_library = hash_map_me_library.clone();
+    fasta_file_read::fasta_reader(
+      &me_library_file,
+      cr_me_library,
+    )?;
+
+    // write to cache
+    let cw_me_library = hash_map_me_library.clone();
+    fasta_cache_write::write_cache(
+      &me_library_file,
+      cw_me_library,
+    )?;
+
+  }
 
   // load mobile element aligned reads
   let me_aligned_file = format!("{}{}", directory, me_aligned_file);
 
   me_aligned::me_identificator(
     &me_aligned_file,
+    hash_map_me_library,
     hash_map_collection,
-    &me_collection,
   )?;
 
   Ok(())
