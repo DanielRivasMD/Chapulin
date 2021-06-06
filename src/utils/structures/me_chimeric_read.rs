3aad043d82d5297d946791226b28086221df55ce
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-06 12:51:26 +0200

    add mobile element chimeric read display

--- src/utils/structures/me_chimeric_read.rs
+++ src/utils/structures/me_chimeric_read.rs
@@ -55,3 +55,28 @@ impl MEChimericRead {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use std::fmt;
+
+impl fmt::Display for MEChimericRead {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+    writeln!(
+      f,
+      "Chromosome: {}, Position: {}",
+      self.chr_read[0].chr,
+      self.chr_read[0].pos,
+    )
+  }
+}
+
+  // #[new(default)]
+  // pub sequence: String,
+  // #[new(default)]
+  // pub me_read: Vec<MEAnchor>,
+  // #[new(default)]
+  // pub chr_read: Vec<ChrAnchor>,
+  // #[new(default)]
+  // pub breakpoint: BreakPoint,
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
