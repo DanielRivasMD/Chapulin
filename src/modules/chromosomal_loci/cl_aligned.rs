ca9441977c3c22f6b9f5baba81b9b9c241d33c5e
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-06 12:49:36 +0200

    update chromosomal location aligned changes

--- src/modules/chromosomal_loci/cl_aligned.rs
+++ src/modules/chromosomal_loci/cl_aligned.rs
@@ -1,41 +1,31 @@
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // standard libraries
-use std::collections::{HashMap};
-use std::sync::{Arc, Mutex};
-use std::str::{from_utf8};
-use std::fs::{File};
-use std::io::{Write};
-use anyhow::{Context};
+use anyhow::Context;
 use anyhow::Result as anyResult;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use std::str::from_utf8;
+use std::sync::{Arc, Mutex};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
 use crate::{
-  utils::functions::{
-    file_reader::byte_file_reader,
-  },
+  settings::constants::MAPQ,
+  utils::functions::file_reader::byte_file_reader,
   utils::structures::{
-    me_chimeric_pair::MEChimericPair,
-    chr_anchor::ChrAnchor,
-    chr_anchor_enum::ChrAnchorEnum,
-  },
-  settings::{
-    constants::MAPQ,
+    chr_anchor::ChrAnchor, chr_anchor_enum::ChrAnchorEnum, me_chimeric_pair::MEChimericPair,
   },
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // error handler
-use crate::error::{
-  common_error::ChapulinCommonError,
-};
+use crate::error::common_error::ChapulinCommonError;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 pub fn cl_mapper(
   cl_bam_file: &str,
@@ -43,16 +33,14 @@ pub fn cl_mapper(
   hm_collection: Arc<Mutex<HashMap<String, MEChimericPair>>>,
   an_registry: Arc<Mutex<HashMap<String, Vec<String>>>>,
 ) -> anyResult<()> {
-
   let fl_write = format!("{}.err", errata);
-  let mut fl = File::create(&fl_write).context(ChapulinCommonError::CreateFile{ f: fl_write })?;
+  let mut fl = File::create(&fl_write).context(ChapulinCommonError::CreateFile { f: fl_write })?;
 
   // load file
   let mut lines = byte_file_reader(&cl_bam_file)?;
 
   // iterate through file
   while let Some(line) = lines.next() {
-
     let record_line: Vec<&str> = from_utf8(&line?)
       .context(ChapulinCommonError::RegistryLine)?
       .trim()
@@ -63,54 +51,45 @@ pub fn cl_mapper(
     let read_id = record_line[0].to_string();
     let chr = record_line[2].to_string();
 
-    if hm_collection
-      .lock().unwrap()
-      .contains_key(&read_id) {
+    // TODO: read supplementary fields for additional information & load on struct
 
+    if hm_collection.lock().unwrap().contains_key(&read_id) {
       let mut mapq_switch = false;
 
-      if let Some(current_record) = hm_collection
-        .lock().unwrap()
-        .get_mut(&read_id) {
-
+      if let Some(current_record) = hm_collection.lock().unwrap().get_mut(&read_id) {
         reload!(current_record, read1, record_line);
         reload!(current_record, read2, record_line);
 
         match current_record.chranch {
-          ChrAnchorEnum::Read1 => { mapq_switch = mapq!(current_record, read1); },
-          ChrAnchorEnum::Read2 => { mapq_switch = mapq!(current_record, read2); },
+          ChrAnchorEnum::Read1 => {
+            mapq_switch = mapq!(current_record, read1);
+          }
+          ChrAnchorEnum::Read2 => {
+            mapq_switch = mapq!(current_record, read2);
+          }
           _ => (),
         };
-
       }
 
-// TODO: consider tagging strand on the fly to avoid postload counting
+      // TODO: consider tagging strand on the fly to avoid postload counting
       if mapq_switch {
-        hm_collection
-          .lock().unwrap()
-          .remove(&read_id);
+        hm_collection.lock().unwrap().remove(&read_id);
       } else {
         // register chromosome anchors
-        if ! an_registry
-          .lock().unwrap()
-          .contains_key(&chr) {
-          an_registry
-            .lock().unwrap()
-            .insert(chr.clone(), Vec::new());
+        if !an_registry.lock().unwrap().contains_key(&chr) {
+          an_registry.lock().unwrap().insert(chr.clone(), Vec::new());
         }
 
-        if let Some(current_chr) = an_registry
-          .lock().unwrap()
-          .get_mut(&chr) {
-          if ! current_chr.contains(&read_id.to_string()) {
+        if let Some(current_chr) = an_registry.lock().unwrap().get_mut(&chr) {
+          if !current_chr.contains(&read_id.to_string()) {
             current_chr.push(read_id.to_string())
           }
         }
       }
-
-    //   ic!(record_line);
     } else {
-      fl.write_all(record_line[0].to_string().as_bytes()).context(ChapulinCommonError::WriteFile{ f: record_line[0].to_string() })?;
+      // TODO: all records are going here. investigate the reason
+      // ic!(record_line);
+      // fl.write_all(record_line[0].to_string().as_bytes()).context(ChapulinCommonError::WriteFile{ f: record_line[0].to_string() })?;
     }
   }
 
