f238c85a230d9980d45f2378757074ca3e95e2ed
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-06 12:51:43 +0200

    update chapulin draft changes

--- src/chapulin.rs
+++ src/chapulin.rs
@@ -1,4 +1,3 @@
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // macros                                                                                         //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -17,18 +16,37 @@ extern crate lazy_static;
 #[macro_use]
 mod macros;
 
-// // debug
-// #[macro_use]
-// extern crate icecream;
-
+// debug
+#[macro_use]
+extern crate icecream;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // modules
-pub mod settings;
-pub mod utils;
-pub mod modules;
 pub mod controllers;
 pub mod error;
+pub mod modules;
+pub mod settings;
+pub mod utils;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// use prettytable::format::LineSeparator;
+// use prettytable::format::LinePosition;
+// use prettytable::format::FormatBuilder;
+// use prettytable::format::TableFormat;
+
+// lazy_static! {
+//     static ref COOL_SEP: LineSeparator = LineSeparator::new('\u{2256}', '\u{2256}', '\u{2256}', '\u{2256}');
+
+//     pub static ref COOL_FORMAT: TableFormat = FormatBuilder::new()
+//       .column_separator('\u{22EE}')
+//       .borders('\u{22EE}')
+//       .separator(LinePosition::Title, *COOL_SEP)
+//       .separator(LinePosition::Bottom, *COOL_SEP)
+//       .separator(LinePosition::Top, *COOL_SEP)
+//       .padding(1, 1)
+//       .build();
+// }
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
