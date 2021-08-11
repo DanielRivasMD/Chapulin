34ed035f4c5a4b3fa5cb39f1b6bbd620e215cd11
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-11 18:18:19 +0200

    refactor fasta file read test functional

--- tests/modules/fasta_read/fasta_file_read.rs
+++ tests/modules/fasta_read/fasta_file_read.rs
@@ -15,39 +15,41 @@ use chapulin::modules::fasta_read::fasta_file_read::fasta_reader;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+fn mux(ref_seq: &str) -> Arc<Mutex<HashMap<String, f64>>> {
+  // declare arc mutex & clone
+  let mutex_ref_seq = Arc::new(Mutex::new(HashMap::new()));
+  let clone_mutex = Arc::clone(&mutex_ref_seq);
+
+  // read fasta
+  // observe that error is unwrap
+  fasta_reader(ref_seq, mutex_ref_seq).unwrap();
+
+  clone_mutex
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 data_test! {
 
   fn test_fasta_reader(ref_seq, key, val) => {
-    // declare arc mutex & clone
-    let mutex_ref_seq = super::Arc::new(super::Mutex::new(super::HashMap::new()));
-    let clone_mutex = super::Arc::clone(&mutex_ref_seq);
-
-    // read fasta
-    // observe that error is unwrap
-    super::fasta_reader(ref_seq, mutex_ref_seq).unwrap();
-
+    // declare values
+    let clone_mutex = super::mux(ref_seq);
     // assert
     assert_eq!(clone_mutex.lock().unwrap().get(key), Some(&val));
   }
 
-  - fasta1("tests/samples/dummy_ref.fa", "1", 54.)
-  - fasta2("tests/samples/dummy_ref.fa", "4", 14.)
+  - _01("tests/samples/dummy_ref.fa", "1", 54.)
+  - _02("tests/samples/dummy_ref.fa", "4", 14.)
 
   fn fail_fasta_reader(ref_seq, key, val) => {
-    // declare arc mutex & clone
-    let mutex_ref_seq = super::Arc::new(super::Mutex::new(super::HashMap::new()));
-    let clone_mutex = super::Arc::clone(&mutex_ref_seq);
-
-    // read fasta
-    // observe that error is unwrap
-    super::fasta_reader(ref_seq, mutex_ref_seq).unwrap();
-
+    // declare values
+    let clone_mutex = super::mux(ref_seq);
     // assert
     assert_ne!(clone_mutex.lock().unwrap().get(key), Some(&val));
   }
 
-  - fasta1("tests/samples/dummy_ref.fa", "1", 50.)
-  - fasta2("tests/samples/dummy_ref.fa", "4", 4.)
+  - _01("tests/samples/dummy_ref.fa", "1", 50.)
+  - _02("tests/samples/dummy_ref.fa", "4", 4.)
 
 }
 
