b28a2438783b9c7fd20138497f3e468c1a65d4a8
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-12 12:19:57 +0200

    update test mobile element aligned

--- tests/modules/mobile_elements/me_aligned.rs
+++ tests/modules/mobile_elements/me_aligned.rs
@@ -12,19 +12,17 @@ use std::sync::{
 // crate utilities
 use chapulin::modules::mobile_elements::me_aligned;
 use genomic_structures::{
-  ChrAnchorEnum,
   ExtraValuesEnum,
   MEAnchor,
   MEChimericPair,
   MEChimericRead,
   RawValues,
-  CIGAR,
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 macro_rules! me_aligned {
-  ( $function: ident; $assertion: ident; $result: ident ; $key: expr; $val: expr; $mobel_id: expr; $mobel_size: expr ) => {
+  ( $function: ident; $assertion: ident; $key: expr; $val: expr; $mobel_id: expr; $mobel_size: expr ) => {
     #[test]
     fn $function() {
       // declare files
@@ -34,7 +32,6 @@ macro_rules! me_aligned {
       let mutex_me_collection = Arc::new(Mutex::new(HashMap::new()));
 
       // create mobile element library
-      // let mobel_size = 1000.;
       mutex_me_collection
         .lock()
         .unwrap()
@@ -56,10 +53,7 @@ macro_rules! me_aligned {
       .unwrap();
 
       // assert
-      $assertion!(
-        clone_mutex.lock().unwrap().get($key),
-        $result(&chimeric_pair_make($val))
-      );
+      $assertion!(clone_mutex.lock().unwrap().get($key), $val);
       // $assertion!($key, $val);
     }
   };
@@ -67,44 +61,36 @@ macro_rules! me_aligned {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// read1: Vec<&str>,
-// read2: Vec<&str>,
+// TODO: integrate chimeric pair build into macro
+me_aligned!(test01; assert_ne; "CS0001.1"; None; ""; 0.);
+me_aligned!(test02; assert_eq; "CS0001.1";
+  Some(&chimeric_pair_build(&[
+    &["CS0001.1", "75", "REF_cs100", "1", "37", "100M", "=", "150", "-100", "AGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGACAGGGTTTCACCATGTTGGTTAGGCTGGTCTCAAACTCCTN", "AAAAAEEDAAAAA????;A?A@AAADDDDDDDIIIIIIDIIIIEEIIICIIIECIEEEDIIIIIDEIIEIIIIIIIIIIIIIIIEEBDDD:DDDB=B=1%", "NM:i:5  MD:Z:19G2A4T18C0C10 MC:Z:73S27M AS:i:33 XS:i:33"],
+    &["CS0001.1", "135", "REF_cs100", "150", "37", "100M", "=", "1", "100", "TCCAGGGTTCAAGNGATTCTCCTGCCTCAGCCTCCAGAGTAGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGAC", "BCCFFFFDHHHHH%2AEGIIIIIIIIIIIIIIIIIIIGH<FHIIIGIIIIIIGGI=FCGIIIIIHHCHFFFDAD@A>;ACDDDDB>CFFED?CDDDDDCC", "NM:i:2  MD:Z:19G2A4 MC:Z:33S58M9S AS:i:19 XS:i:19"]
+    ]));
+"REF_cs100";
+1000.
+);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // SAM line reader
-fn chimeric_pair_make(flines: Vec<Vec<&str>>) -> MEChimericPair {
+fn chimeric_pair_build(flines: &[&[&str]]) -> MEChimericPair {
   // declare mobile element chimeric pair
   let mut chimeric_pair = MEChimericPair::new();
 
-  // load raw values
-  chimeric_pair.read1 = raw_values_load(&flines[0]);
-  chimeric_pair.read2 = raw_values_load(&flines[1]);
-
   // load chimeric read
-
-  // make chr anchor
-  // chimeric_pair.chranch = ChrAnchorEnum::Read2;
-
-  // // load read1 raw values
-  // let raw_values = RawValues::load(read1).unwrap();
-
-  // // load read2 raw values
-  // let raw_values = RawValues::load(read2).unwrap();
-
-  // raw_values.update(flines).unwrap();
-  // let mut cigar = CIGAR::new();
-
-  // let mut chimeric_read1 = MEChimericRead::new();
-  // let mut chimeric_read2 = MEChimericRead::new();
-  // let mut chranch = ChrAnchorEnum::new();
+  chimeric_pair.read1 = chimeric_read_build(&flines[0]);
+  chimeric_pair.read2 = chimeric_read_build(&flines[1]);
 
   chimeric_pair
 }
 
-// raw_values: RawValues,
-// chimeric_pair: MEChimericPair,
-fn raw_values_load(flines: &[&str]) -> MEChimericRead {
+fn chimeric_read_build(flines: &[&str]) -> MEChimericRead {
   // load raw values
   let mut raw_values = RawValues::load(flines.to_vec()).unwrap();
 
+  // hardcoded
   raw_values.extra = ExtraValuesEnum::MobelSize(1000.);
 
   // construct chimeric read
@@ -125,19 +111,5 @@ fn raw_values_load(flines: &[&str]) -> MEChimericRead {
 
   chimeric_read
 }
-
-// me_aligned!(test01; assert_eq; CIGAR::new(); CIGAR::new());
-// me_aligned!(fail01; assert_ne; CIGAR::load("100M", 100).unwrap();
-// CIGAR::new());
-
-// me_aligned!(test01; assert_eq; "SRR556146.1"; None; ""; ""; 0.);
-me_aligned!(test02; assert_eq; Some; "CS0001.1";
-  vec![
-    vec!["CS0001.1", "75", "REF_cs100", "1", "37", "100M", "=", "150", "-100", "AGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGACAGGGTTTCACCATGTTGGTTAGGCTGGTCTCAAACTCCTN", "AAAAAEEDAAAAA????;A?A@AAADDDDDDDIIIIIIDIIIIEEIIICIIIECIEEEDIIIIIDEIIEIIIIIIIIIIIIIIIEEBDDD:DDDB=B=1%", "NM:i:5  MD:Z:19G2A4T18C0C10 MC:Z:73S27M AS:i:33 XS:i:33"],
-    vec!["CS0001.1", "135", "REF_cs100", "150", "37", "100M", "=", "1", "100", "TCCAGGGTTCAAGNGATTCTCCTGCCTCAGCCTCCAGAGTAGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGAC", "BCCFFFFDHHHHH%2AEGIIIIIIIIIIIIIIIIIIIGH<FHIIIGIIIIIIGGI=FCGIIIIIHHCHFFFDAD@A>;ACDDDDB>CFFED?CDDDDDCC", "NM:i:2  MD:Z:19G2A4 MC:Z:33S58M9S AS:i:19 XS:i:19"]
-    ];
-"REF_cs100";
-1000.
-);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
