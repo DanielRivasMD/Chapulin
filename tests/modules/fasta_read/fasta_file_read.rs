0c332ba8bb892f6ad7d30e50d67609d4eb971985
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-12 12:23:49 +0200

    update test fasta read

--- tests/modules/fasta_read/fasta_file_read.rs
+++ tests/modules/fasta_read/fasta_file_read.rs
@@ -10,21 +10,24 @@ use std::sync::{
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
-use chapulin::modules::fasta_read::fasta_file_read::fasta_reader;
+use chapulin::modules::fasta_read::fasta_file_read;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-macro_rules! fasta_file_read {
-  ( $func: ident; $assertion: ident; $key: expr; $val: expr ) => {
+macro_rules! fasta_read {
+  ( $function: ident; $assertion: ident; $key: expr; $val: expr ) => {
     #[test]
-    fn $func() {
+    fn $function() {
+      // declare file
       let ref_seq = "tests/samples/dummy_ref.fa";
+
+      // declare mutex
       let mutex_ref_seq = Arc::new(Mutex::new(HashMap::new()));
       let clone_mutex = Arc::clone(&mutex_ref_seq);
 
       // read fasta
       // observe that error is unwrap
-      fasta_reader(ref_seq, mutex_ref_seq).unwrap();
+      fasta_file_read::fasta_read(ref_seq, mutex_ref_seq).unwrap();
 
       // assert
       $assertion!(clone_mutex.lock().unwrap().get($key), $val);
@@ -34,11 +37,24 @@ macro_rules! fasta_file_read {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-fasta_file_read!(test01; assert_eq; "1"; Some(&54.)); // value 1
-fasta_file_read!(test02; assert_eq; "4"; Some(&14.)); // value 2
-fasta_file_read!(test03; assert_eq; "10"; None); // no value
-fasta_file_read!(fail01; assert_ne; "4"; None); // value 1
-fasta_file_read!(fail02; assert_ne; "1"; Some(&504.)); // value 2
-fasta_file_read!(fail03; assert_ne; "10"; Some(&0.)); // no value
+// tests
+
+// no value
+fasta_read!(test00; assert_eq; "10"; None);
+
+// value 1
+fasta_read!(test01; assert_eq; "1"; Some(&54.));
+
+// value 2
+fasta_read!(test02; assert_eq; "4"; Some(&14.));
+
+// no value
+fasta_read!(fail03; assert_ne; "10"; Some(&0.));
+
+// value 1
+fasta_read!(fail01; assert_ne; "4"; None);
+
+// value 2
+fasta_read!(fail02; assert_ne; "1"; Some(&504.));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
