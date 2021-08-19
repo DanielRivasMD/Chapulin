8998308fc294cc5842e1debd3721f5272a5fc524
DanielRivasMD <danielrivasmd@gmail.com>
2021-08-19 09:25:42 +0200

    update test mobile element aligned comments & samples

--- tests/modules/mobile_elements/me_aligned.rs
+++ tests/modules/mobile_elements/me_aligned.rs
@@ -79,95 +79,89 @@ macro_rules! me_aligned {
 // TODO: update sample file. values below are not kept by algorithm
 // tests
 
-// no value
-me_aligned!(test00; assert_eq; "Random_ID"; None; ""; 0.);
-
-// // mount value through function
-// me_aligned!(test02; assert_eq; "CS0001.1";
-//   Some(&chimeric_pair_build(&[
-//     &["CS0001.1", "75", "REF_cs100", "1", "37", "100M", "=", "150", "-100",
-// "AGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGACAGGGTTTCACCATGTTGGTTAGGCTGGTCTCAAACTCCTN"
-// , "AAAAAEEDAAAAA????;A?A@
-// AAADDDDDDDIIIIIIDIIIIEEIIICIIIECIEEEDIIIIIDEIIEIIIIIIIIIIIIIIIEEBDDD:
-// DDDB=B=1%", "NM:i:5  MD:Z:19G2A4T18C0C10 MC:Z:73S27M AS:i:33 XS:i:33"],
-//     &["CS0001.1", "135", "REF_cs100", "150", "37", "100M", "=", "1", "100",
-// "TCCAGGGTTCAAGNGATTCTCCTGCCTCAGCCTCCAGAGTAGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGAC"
-// , "BCCFFFFDHHHHH%
-// 2AEGIIIIIIIIIIIIIIIIIIIGH<FHIIIGIIIIIIGGI=FCGIIIIIHHCHFFFDAD@A>;
-// ACDDDDB>CFFED?CDDDDDCC", "NM:i:2  MD:Z:19G2A4 MC:Z:33S58M9S AS:i:19 XS:i:19"]
-//     ]));
-// "REF_cs100";
-// 1000.
+// // no value
+// me_aligned!(test00;
+//   mobile |> "".to_string(), 0.;
+//   params |> "RandomID", None;
 // );
 
-// // mount value manually
-// me_aligned!(test03; assert_eq; "CS0001.1";
-//   Some(&MEChimericPair{
-//     read1: MEChimericRead{
-//       breakpoint: BreakPoint{
-//         sequence: String::new(),
-//         coordinate: 0,
-//       },
-//       chr_read: vec![],
-//       me_read: vec![
-//         MEAnchor{
-//           cigar: CIGAR{
-//             align: vec![100],
-//             del: vec![],
-//             ins: vec![],
-//             lclip: 0,
-//             left_boundry: 1,
-//             rclip: 0,
-//             right_boundry: 101,
-//             signature: String::from("100M"),
-//           },
-//           flag: 75,
-//           mobel: String::from("REF_cs100"),
-//           orientation: OrientationEnum::None,
-//           position: 1,
-//           size: 1000.0
-//         }
-//       ],
-//       quality: 37,
-//       sequence:
-// String::from("
-// AGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGACAGGGTTTCACCATGTTGGTTAGGCTGGTCTCAAACTCCTN"
-// ),     },
-//     read2: MEChimericRead{
-//       breakpoint: BreakPoint{
-//         sequence: String::new(),
-//         coordinate: 0
-//       },
-//       chr_read: vec![],
-//       me_read: vec![
-//         MEAnchor{
-//           cigar: CIGAR{
-//             align: vec![100],
-//             del: vec![],
-//             ins: vec![],
-//             lclip: 0,
-//             left_boundry: 150,
-//             rclip: 0,
-//             right_boundry: 250,
-//             signature: String::from("100M"),
-//           },
-//           flag: 135,
-//           mobel: String::from("REF_cs100"),
-//           orientation: OrientationEnum::None,
-//           position: 150,
-//           size: 1000.0
-//         }
-//       ],
-//       quality: 37,
-//       sequence:
-// String::from("
-// TCCAGGGTTCAAGNGATTCTCCTGCCTCAGCCTCCAGAGTAGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGAC"
-// ),     },
-//     chranch: ChrAnchorEnum::None,
-//   });
-// "REF_cs100";
-// 1000.
-// );
+// mount value through function
+me_aligned!(test01;
+  mobile |> "mobel11000".to_string(), 11000.;
+  params |> "KEEP01.1",
+  Some(&chimeric_pair_build(
+    &[
+      &["KEEP01.1", "83", "mobel11000", "1", "37", "100M", "=", "150", "-100", "AGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGACAGGGTTTCACCATGTTGGTTAGGCTGGTCTCAAACTCCTN", "AAAAAEEDAAAAA????;A?A@AAADDDDDDDIIIIIIDIIIIEEIIICIIIECIEEEDIIIIIDEIIEIIIIIIIIIIIIIIIEEBDDD:DDDB=B=1%", "NM:i:5  MD:Z:19G2A4T18C0C10 MC:Z:73S27M AS:i:33 XS:i:33"],
+      &["KEEP01.1", "163", "mobel11000", "150", "37", "100M", "=", "1", "100", "TCCAGGGTTCAAGNGATTCTCCTGCCTCAGCCTCCAGAGTAGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGAC", "BCCFFFFDHHHHH%2AEGIIIIIIIIIIIIIIIIIIIGH<FHIIIGIIIIIIGGI=FCGIIIIIHHCHFFFDAD@A>;ACDDDDB>CFFED?CDDDDDCC", "NM:i:2  MD:Z:19G2A4 MC:Z:33S58M9S AS:i:19 XS:i:19"],
+    ],
+    11000.,
+  ));
+);
+
+// mount value manually
+me_aligned!(test02;
+  mobile |> "mobel11000".to_string(), 11000.;
+  params |> "KEEP01.1",
+  Some(&MEChimericPair{
+    read1: MEChimericRead{
+      chr_read: vec![],
+      me_read: vec![
+        MEAnchor{
+          breakpoint: BreakPoint{
+            sequence: String::new(),
+            coordinate: 0.0,
+          },
+          cigar: CIGAR{
+            align: vec![100],
+            deletion: vec![],
+            insertion: vec![],
+            left_boundry: 1,
+            left_clip: 0,
+            right_boundry: 100,
+            rigth_clip: 0,
+            signature: "100M".to_string(),
+          },
+          flag: 83,
+          mobel: "mobel11000".to_string(),
+          orientation: OrientationEnum::Upstream,
+          position: 1,
+          size: 11000.
+        }
+      ],
+      quality: 37,
+      sequence: "AGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGACAGGGTTTCACCATGTTGGTTAGGCTGGTCTCAAACTCCTN".to_string(),
+    },
+    read2: MEChimericRead{
+      chr_read: vec![],
+      me_read: vec![
+        MEAnchor{
+          breakpoint: BreakPoint{
+            sequence: String::new(),
+            coordinate: 0.0
+          },
+          cigar: CIGAR{
+            align: vec![100],
+            deletion: vec![],
+            insertion: vec![],
+            left_boundry: 150,
+            left_clip: 0,
+            right_boundry: 249,
+            rigth_clip: 0,
+            signature: "100M".to_string(),
+          },
+          flag: 163,
+          mobel: "mobel11000".to_string(),
+          orientation: OrientationEnum::None,
+          position: 150,
+          size: 11000.
+        }
+      ],
+      quality: 37,
+      sequence: "TCCAGGGTTCAAGNGATTCTCCTGCCTCAGCCTCCAGAGTAGCTGAGACTACAGGTGTCCGCCACCAGGCCCAGCTAATTTTTGTATTTTTATTAGAGAC".to_string(),
+    },
+    chranch: ChrAnchorEnum::Read2,
+  });
+);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -186,6 +180,7 @@ fn chimeric_pair_build(
   // FIX: HARDCODED anchor tag. write tag method on mobile element chimeric pair
   chimeric_pair.chranch = ChrAnchorEnum::Read2;
 
+  // return
   chimeric_pair
 }
 
@@ -202,7 +197,7 @@ fn chimeric_read_build(
   // construct chimeric read
   let mut chimeric_read = MEChimericRead::new();
 
-  // mobile element
+  // load mobile element
   chimeric_read.me_read.push(MEAnchor::load(
     raw_values.cigar.clone(),
     raw_values.flag,
@@ -218,9 +213,11 @@ fn chimeric_read_build(
     .iter_mut()
     .for_each(|me_anchor| me_anchor.tag());
 
+  // load mapq & sequence
   chimeric_read.quality = raw_values.quality;
   chimeric_read.sequence = raw_values.sequence;
 
+  // return
   chimeric_read
 }
 
